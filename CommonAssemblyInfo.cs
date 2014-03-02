2081d64fe7a9570fbc3c0a367452269e38b3966e
Simon Cropp <simon.cropp@gmail.com>
2014-03-02 11:14:43 +1100

    dev dep

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Ionad")]
 [assembly: AssemblyProduct("Ionad")]
-[assembly: AssemblyVersion("0.2.3")]
-[assembly: AssemblyFileVersion("0.2.3")]
+[assembly: AssemblyVersion("0.2.4")]
+[assembly: AssemblyFileVersion("0.2.4")]
